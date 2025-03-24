738ab5866bd15453eb8d679a2c0f2be07c475d5d
karthikinfo2001 <karthikinfo2001@gmail.com>
2025-03-24 13:19:28 +0530

    This is three commit

--- qadb.sql
+++ qadb.sql
@@ -1,5 +1,7 @@
-CREATE TABLE northschool (
-    name VARCHAR(100) NOT NULL,
-    phone VARCHAR(15),
-    country VARCHAR(50)
-);
+--CREATE TABLE northschool (
+   -- name VARCHAR(100) NOT NULL,
+   -- phone VARCHAR(15),
+   -- country VARCHAR(50)
+--);
+
+alter table qadb add column address varchar;
